d6387c1f875653b64f6c63103199144d0233d166
tanjetsheng <tanjetsheng@gmail.com>
2017-07-02 12:00:19 +0800

    removed

--- test/test_linkedlist.c
+++ test/test_linkedlist.c
@@ -1,33 +1,6 @@
 #include "unity.h"
 #include "linkedlist.h"
 #include "student.h"
-
-void setUp(void)
-{
-}
-
-void tearDown(void)
-{
-}
-
-void test_listInit_ensure_initialized_to_NULL_and_0(void){
-  LinkedList list = {
-    (Item *)-1,
-    (Item *)-1,
-    10
-  };
-  
-  listInit(&list);
-  
-  TEST_ASSERT_NULL(list.head);
-  TEST_ASSERT_NULL(list.tail);
-  TEST_ASSERT_EQUAL(0,list.len);
-  
-    
-}
-
-void test_listAdd_given_linked_list_with_ali_then_add_baba_expect_2_items_in_list(void){
-  LinkedList list;
    struct Student Ali = {
       "Ali",                        //name
       23,                           //age
@@ -58,6 +31,33 @@ void test_listAdd_given_linked_list_with_ali_then_add_baba_expect_2_items_in_lis
       (Item *)-1,                   //next
       (void *)&Abu                //data
     };
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_listInit_ensure_initialized_to_NULL_and_0(void){
+  LinkedList list = {
+    (Item *)-1,
+    (Item *)-1,
+    10
+  };
+  
+  listInit(&list);
+  
+  TEST_ASSERT_NULL(list.head);
+  TEST_ASSERT_NULL(list.tail);
+  TEST_ASSERT_EQUAL(0,list.len);
+  
+    
+}
+
+void test_listAdd_given_linked_list_with_ali_then_add_baba_expect_2_items_in_list(void){
+  LinkedList list;
+
   
      printf("address of itemali: %p\n", &itemali);
      printf("address of itembaba: %p\n", &itembaba);
@@ -69,7 +69,7 @@ void test_listAdd_given_linked_list_with_ali_then_add_baba_expect_2_items_in_lis
   listAdd(&list,&itemAbu);
 
   TEST_ASSERT_EQUAL_PTR(&itemali,list.head);
-  TEST_ASSERT_EQUAL_PTR(&itembaba,list.tail);
+  TEST_ASSERT_EQUAL_PTR(&itemAbu,list.tail);
   TEST_ASSERT_EQUAL(3,list.len);
   TEST_ASSERT_EQUAL_PTR(&Ali,itemali.data);
   TEST_ASSERT_EQUAL_PTR(&Baba,itembaba.data);
@@ -78,31 +78,44 @@ void test_listAdd_given_linked_list_with_ali_then_add_baba_expect_2_items_in_lis
 
   
 }
-/*  
-void test_listAdd_given_an_empty_linked_list_then_add_Ali_expect_1_item_in_list(void)
-{
-  
-  LinkedList list;
-    Student Ali = {
-      "Ali",                        //name
-      23,                           //age
-      72.35,                        //weight
-      1.78                          //height  
-    };
-    Item item = {
-      (Item *)-1,                   //next
-      (void *)&Ali                 //data
-    };
- 
-    printf("address of ite: %p\n", &item);
-    
-    listInit(&list);
-    listAdd(&list,&item);
-    TEST_ASSERT_EQUAL_PTR(&item,list.head);    
-    TEST_ASSERT_EQUAL_PTR(&item,list.tail);   
-    TEST_ASSERT_EQUAL(1,list.len);
-    TEST_ASSERT_NULL(item.next);
-    TEST_ASSERT_EQUAL_PTR(&Ali,item.data);
-    
+
+
+void test_remove_expect_Ali_and_Baba(void){
+	LinkedList list;
+	listInit(&list);
+	listAdd(&list,&itemali);
+	listAdd(&list,&itembaba);
+	listAdd(&list,&itemAbu);
+	LinkRemove(&list,"Abu");
+	TEST_ASSERT_EQUAL_PTR(&itemali,list.head);
+	TEST_ASSERT_EQUAL_PTR(&itembaba,list.tail);
+	TEST_ASSERT_EQUAL(2,list.len);
+	TEST_ASSERT_NULL(itembaba.next);
+
 }
-*/
+
+
+void test_remove_expect_Ali_and_Abu(void){
+	LinkedList list;
+	listInit(&list);
+	listAdd(&list,&itemali);
+	listAdd(&list,&itembaba);
+	listAdd(&list,&itemAbu);
+	LinkRemove(&list,"Baba");
+	TEST_ASSERT_EQUAL_PTR(&itemali,list.head);
+	TEST_ASSERT_EQUAL_PTR(&itemAbu,list.tail);
+	TEST_ASSERT_EQUAL(2,list.len);
+	TEST_ASSERT_NULL(itemAbu.next);
+}
+
+
+void test_remove_From2data_expect_Baba(void){
+	LinkedList list;
+	listInit(&list);
+	listAdd(&list,&itembaba);
+	listAdd(&list,&itemAbu);
+	LinkRemove(&list,"Abu");
+	TEST_ASSERT_EQUAL_PTR(&itembaba,list.head);
+	TEST_ASSERT_EQUAL(1,list.len);
+	TEST_ASSERT_NULL(itembaba.next);
+}
